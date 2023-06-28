6a6b4c118e13259a19329407e79ba14d6d0ee880
titusfortner <titus.fortner@gmail.com>
2023-06-27 20:44:35 -0500

    let java compile for 4.7

--- examples/java/src/test/java/dev/selenium/bidirectional/log/LogInspectorTest.java
+++ examples/java/src/test/java/dev/selenium/bidirectional/log/LogInspectorTest.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.bidi.LogInspector;
 import org.openqa.selenium.bidi.log.ConsoleLogEntry;
 import org.openqa.selenium.bidi.log.JavascriptLogEntry;
-import org.openqa.selenium.bidi.log.LogLevel;
+//import org.openqa.selenium.bidi.log.LogLevel;
 import org.openqa.selenium.bidi.log.StackTrace;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
@@ -46,23 +46,23 @@ class LogInspectorTest extends BaseTest {
         }
     }
 
-    @Test
-    void testListenToJavascriptLog()
-            throws ExecutionException, InterruptedException, TimeoutException {
-        try (LogInspector logInspector = new LogInspector(driver)) {
-            CompletableFuture<JavascriptLogEntry> future = new CompletableFuture<>();
-            logInspector.onJavaScriptLog(future::complete);
-
-            driver.get("https://www.selenium.dev/selenium/web/bidi/logEntryAdded.html");
-            driver.findElement(By.id("jsException")).click();
-
-            JavascriptLogEntry logEntry = future.get(5, TimeUnit.SECONDS);
-
-            Assertions.assertEquals("Error: Not working", logEntry.getText());
-            Assertions.assertEquals("javascript", logEntry.getType());
-            Assertions.assertEquals(LogLevel.ERROR, logEntry.getLevel());
-        }
-    }
+//    @Test
+//    void testListenToJavascriptLog()
+//            throws ExecutionException, InterruptedException, TimeoutException {
+//        try (LogInspector logInspector = new LogInspector(driver)) {
+//            CompletableFuture<JavascriptLogEntry> future = new CompletableFuture<>();
+//            logInspector.onJavaScriptLog(future::complete);
+//
+//            driver.get("https://www.selenium.dev/selenium/web/bidi/logEntryAdded.html");
+//            driver.findElement(By.id("jsException")).click();
+//
+//            JavascriptLogEntry logEntry = future.get(5, TimeUnit.SECONDS);
+//
+//            Assertions.assertEquals("Error: Not working", logEntry.getText());
+//            Assertions.assertEquals("javascript", logEntry.getType());
+//            Assertions.assertEquals(LogLevel.ERROR, logEntry.getLevel());
+//        }
+//    }
 
     @Test
     void testListenToJavascriptErrorLog()
